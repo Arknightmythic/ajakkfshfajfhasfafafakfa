77f32ed3ce3c5c79a3b7f5bed33163619b5d6439
patricia <patricia.laurencia@is-gs.com>
2025-06-25 16:03:52 +0700

    fix loading in App.jsx and ListRoutes.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,11 @@ function App() {
   return (
     <Suspense
       fallback={
-        <div className='text-center mt-10'>
-          <Loader2 className='animate-spin w-8 h-8 mx-auto mb-4 text-blue-600' />
-          <p className='text-gray-600'>Loading...</p>
+        <div className='p-6 flex items-center justify-center min-h-[400px]'>
+          <div className='text-center'>
+            <Loader2 className='animate-spin w-8 h-8 mx-auto mb-4 text-blue-600' />
+            <p className='text-gray-600'>Loading...</p>
+          </div>
         </div>
       }
     >
--- src/routes/ListRoutes.jsx
+++ src/routes/ListRoutes.jsx
@@ -8,10 +8,12 @@ const generateRoutes = (menu) => {
       element: item.component ? (
         <Suspense 
           fallback={
-          <div className='text-center mt-10'>
-            <Loader2 className='animate-spin w-8 h-8 mx-auto mb-4 text-blue-600' />
-            <p className='text-gray-600'>Loading...</p>
-          </div>
+            <div className='p-6 flex items-center justify-center min-h-[400px]'>
+              <div className='text-center'>
+                <Loader2 className='animate-spin w-8 h-8 mx-auto mb-4 text-blue-600' />
+                <p className='text-gray-600'>Loading...</p>
+              </div>
+            </div>
         }>
           <item.component />
         </Suspense>
